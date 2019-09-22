2abb6eb5c4d3470b1b97270cff7f66a5db71986f
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2019-09-22 16:19:35 +0530

    error

--- orgHead/orgHead/urls.py
+++ orgHead/orgHead/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('headAdmin', include('headSupervisor.urls')),
-    path('accounts',include('accounts.urls')),
-    path('',include('accounts.urls'))
+    path('headAdmin/', include('headSupervisor.urls')),
+    path('accounts/',include('accounts.urls')),
+#    path('',include('accounts.urls'))
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
