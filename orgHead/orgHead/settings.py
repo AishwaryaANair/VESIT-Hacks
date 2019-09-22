de0f6ddfebc54231437eaf6cfe652a9ad511a007
Vedant Sonje <vedant.sonje12@gmail.com>
2019-09-22 15:26:45 +0530

    Kuch Toh hai

--- orgHead/orgHead/settings.py
+++ orgHead/orgHead/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'headSupervisor'
-    'accounts'
+    'headSupervisor',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 LOGIN_REDIRECT_URL = '/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
