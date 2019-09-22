ee4f7816edfaa7346399b791f8a5937166bbff75
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2019-09-23 00:40:01 +0530

    Working Database

--- orgHead/accounts/admin.py
+++ orgHead/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Teams, UserReport, Profile, ProjectReport, Projects
 
 # Register your models here.
+
+admin.site.register(Teams)
+admin.site.register(UserReport)
+admin.site.register(Profile)
+
