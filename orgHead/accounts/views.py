675fbd343a5b23abaf7caa54a4aad6cac2f83a67
sanket <2017.sanket.mahale@ves.ac.in>
2019-09-22 16:46:55 +0530

    emp page

--- orgHead/accounts/views.py
+++ orgHead/accounts/views.py
@@ -6,5 +6,5 @@ def login(request):
     return render(request,'accounts/signandsignup.html')
 
 def home(request): 
-    return render(request,'/home.html')
+    return render(request,'home.html')
     
