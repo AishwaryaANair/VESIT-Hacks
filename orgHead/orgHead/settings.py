c67e0ade06aa68fab1373f36dd7357623c03dbca
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2019-09-22 17:05:04 +0530

    Files for TeamDash

--- orgHead/orgHead/settings.py
+++ orgHead/orgHead/settings.py
@@ -121,9 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = "/orgHead/static"
